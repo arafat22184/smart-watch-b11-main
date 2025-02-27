0caccfd80f64838ed1fb80d1cbaa213455f2e155
arafat22184 <123alarafat@gmail.com>
2025-02-28 03:16:08 +0600

    added website  design

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const ringButtons = document.querySelectorAll(".ring-button");
+
+for (let i = 0; i < ringButtons.length; i++) {
+  const ringBtn = ringButtons[i];
+  ringBtn.addEventListener("click", function (event) {
+    const color = event.target.id.replace("-color", "");
+
+    for (let j = 0; j < ringButtons.length; j++) {
+      ringButtons[j].classList.remove("border-purple-600");
+      ringButtons[j].classList.add("border-gray-300");
+    }
+    //color add kortesi
+    event.target.classList.add("border-purple-600");
+    event.target.classList.remove("border-gray-300");
+
+    const productImage = document.getElementById("product-image");
+    // productImage.src = "../images/gray.png";
+    productImage.src = "../images/" + color + ".png";
+  });
+}
