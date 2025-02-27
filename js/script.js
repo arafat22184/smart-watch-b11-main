aaaab01c33ef2c1537725e346794a160a1adb7ba
arafat22184 <123alarafat@gmail.com>
2025-02-28 04:59:43 +0600

    size and price box border added by js

--- js/script.js
+++ js/script.js
@@ -26,17 +26,31 @@ for (let i = 0; i < ringButtons.length; i++){
   ringBtn.addEventListener('click', function (event) {
 
     const color = event.target.id.replace('-color', '');
-    
 
     for (let j = 0; j < ringButtons.length; j++){
       ringButtons[j].classList.remove('border-purple-600');
       ringButtons[j].classList.add('border-gray-600');
     }
 
-    event.target.classList.add('border-purple-600');
     event.target.classList.remove('border-gray-600');
+    event.target.classList.add('border-purple-600');
 
     const productImage = document.getElementById('product-image');
-    productImage.src = "../images/" + color +".png";
+    productImage.src = "../images/" + color + ".png";
   })
+}
+
+function selectWristSize(size) {
+  const sizes = ['S', 'M', 'L', 'XL'];
+  
+  for (let i = 0; i < sizes.length; i++){
+    const button = document.getElementById('size-' + sizes[i]);
+    const element = sizes[i];
+    if (element === size) {
+    button.classList.add('border-purple-600');
+    }
+    else {
+      button.classList.remove('border-purple-600');
+    }
+  }
 }
